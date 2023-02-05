9a26cd8452ef9345aed33ee536257d613ab5829e
dope66 <prince628@naver.com>
2023-02-05 21:56:10 +0900

    ResultData에 제네릭 추가, doAdd에 제네릭 적용

--- src/main/java/com/khw/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/khw/exam/demo/controller/UsrArticleController.java
@@ -27,7 +27,7 @@ public class UsrArticleController {
 	//액션메서드
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
-	public ResultData doAdd(String title, String body) {
+	public ResultData<Article> doAdd(String title, String body) {
 		if(Utility.empty(title)){
 			return ResultData.from("F-1","제목을 입력해주세요");
 		}
@@ -35,7 +35,7 @@ public class UsrArticleController {
 			return ResultData.from("F-2","내용을 입력해주세요");
 		}
 		
-		ResultData writeArticleRd = articleService.writeArticle(title, body);
+		ResultData<Integer> writeArticleRd = articleService.writeArticle(title, body);
 		int id =(int)writeArticleRd.getData1();
 		Article article = articleService.getArticle(id);
 		
--- src/main/java/com/khw/exam/demo/vo/ResultData.java
+++ src/main/java/com/khw/exam/demo/vo/ResultData.java
@@ -2,21 +2,21 @@ package com.khw.exam.demo.vo;
 
 import lombok.Getter;
 
-public class ResultData {
+public class ResultData<DT> {
 	// S-1 성공, F-1 실패
 	@Getter
 	private String resultCode;
 	@Getter
 	private String msg;
 	@Getter
-	private Object data1;
+	private DT data1;
 	// ResultData 오버로딩
-	public static ResultData from(String resultCode, String msg) {
+	public static <DT> ResultData<DT> from(String resultCode, String msg) {
 		return from(resultCode, msg, null);
 	}
 
-	public static ResultData from(String resultCode, String msg, Object data1) {
-		ResultData rd = new ResultData();
+	public static <DT> ResultData<DT> from(String resultCode, String msg, DT data1) {
+		ResultData<DT> rd = new ResultData<>();
 		rd.resultCode = resultCode;
 		rd.msg = msg;
 		rd.data1 = data1;
