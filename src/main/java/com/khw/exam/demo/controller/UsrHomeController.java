237ef0302fbc0a6fcacce54a974d3aa657a0e778
dope66 <prince628@naver.com>
2023-02-08 17:07:19 +0900

    jsp 연결후 메인페이지

---
+++ src/main/java/com/khw/exam/demo/controller/UsrHomeController.java
@@ -0,0 +1,16 @@
+package com.khw.exam.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class UsrHomeController {
+	@RequestMapping("/usr/home/main")
+	public String showMain() {
+		return "usr/home/main";
+	}
+	@RequestMapping("/")
+	public String showRoot() {
+		return "redirect:/usr/home/main";
+	}
+
+}
