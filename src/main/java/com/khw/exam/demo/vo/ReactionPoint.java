3d991a6e29d33a3f7afe3cbceb1945ed1ec54e04
dope66 <prince628@naver.com>
2023-02-24 17:21:45 +0900

    좋아요, 싫어요 기능 구현중(버튼 눌렀을 때 INSERT되는 것 까지)

---
+++ src/main/java/com/khw/exam/demo/vo/ReactionPoint.java
@@ -0,0 +1,23 @@
+package com.khw.exam.demo.vo;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReactionPoint {
+	private int id;
+	private String regDate;
+	private String updateDate;
+	private int memberId;
+	private String relTypeCode;
+	private int relId;
+
+	private int sumReactionPoint;
+	private int goodReactionPoint;
+	private int badReactionPoint;
+}
